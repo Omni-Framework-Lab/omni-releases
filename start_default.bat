156e7cf213bc6f13deaa9263dc13f41067ec584a
mfreiholz <mf@insanefactory.com>
2026-02-02 06:19:16 +0100

    newest binaries.

--- start_default.bat
+++ start_default.bat
@@ -1,4 +1,4 @@
 wt.exe ^
   --title "CMD" -d %~dp0 ; ^
   --title "Host" -d %~dp0 %~dp0\bin\windows_x64\omni_host.exe --config "%~dp0\config\default.xml" ; ^
-  --title "Web" -d %~dp0 %~dp0\bin\windows_x64\omni_web.exe --webapp-root "%~dp0\webui"
+  --title "Web" -d %~dp0 %~dp0\bin\windows_x64\omni_web.exe --webapp-directory "%~dp0\webui"
